31e45235e0a86b4749eb3d16f59c4a0dcc9fab87
= <=>
2018-11-25 18:30:51 +0700

    first commit

---
+++ EXAM2/Data/EXAM2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EXAM2.Models;
+
+namespace EXAM2.Models
+{
+    public class EXAM2Context : DbContext
+    {
+        public EXAM2Context (DbContextOptions<EXAM2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EXAM2.Models.Category> Category { get; set; }
+
+        public DbSet<EXAM2.Models.Product> Product { get; set; }
+    }
+}
---
+++ EXAM2/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EXAM2.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int SupplierID { get; set; }
+        public int CategoryID { get; set; }
+
+        public int QuantityPerUnit { get; set; }
+        public int UnitPrice { get; set; }
+        public string UnitslnStock { get; set; }
+        public string UnitsOnOrder { get; set; }
+        public string ReoderLevel { get; set; }
+        public string Discontinued { get; set; }
+        [ForeignKey("CategoryID")]
+        public Category Category { get; set; }
+       
+    }
+}
